9fffc6a22802c67c328c2dccbef2ba04015f1cf8
Ilan Coulon <ilan.coulon@gmail.com>
2016-07-22 12:22:03 +0200

    Possibilité de désactivation

--- media/js/content.js
+++ media/js/content.js
@@ -1,17 +1,19 @@
 (function() {
     'use strict';
 
-    chrome.runtime.sendMessage({type: 'parameters', properties: ['tag']}, function(response) {
-      $('a:contains("Se connecter")')
-          .css('background-color', '#59b76d')
-          .css('border-bottom', '3px solid #479257')
-          .css('background-image', 'none')
-          .append(' avec le tag "'+response.tag+'"')
-          .click(function (event) {
-            var pathArray = window.location.pathname.split('/');
-            var candidateId = pathArray[pathArray.length - 1];
-            chrome.runtime.sendMessage({type: 'connection', candidateId: candidateId});
-          });
+    chrome.runtime.sendMessage({type: 'parameters', properties: ['tag', 'activated']}, function(response) {
+      if (response.activated) {
+        $('a:contains("Se connecter")')
+            .css('background-color', '#59b76d')
+            .css('border-bottom', '3px solid #479257')
+            .css('background-image', 'none')
+            .append(' avec le tag "'+response.tag+'"')
+            .click(function (event) {
+              var pathArray = window.location.pathname.split('/');
+              var candidateId = pathArray[pathArray.length - 1];
+              chrome.runtime.sendMessage({type: 'connection', candidateId: candidateId});
+            });
+      }
     });
 
 })();
--- media/js/popup.js
+++ media/js/popup.js
@@ -4,17 +4,22 @@
       chrome.runtime.sendMessage({type: 'parameters', properties: ['tag', 'token']}, function(response) {
          $('#tagInput').val(response.tag);
          $('#tokenInput').val(response.token);
+         $('#activatedCheckbox').prop("checked", response.activated);
       });
       $('#settingsForm').submit(function (e) {
         e.preventDefault();
-        console.log('test');
         chrome.storage.local.set({
+          'activated': $('#activatedCheckbox').prop("checked"),
           'tag': $('#tagInput').val(),
           'token': $('#tokenInput').val()
         }, function() {
           window.close();
         });
         return false;
+      });
+      $('#activatedCheckbox').change(function(event) {
+        $('#tokenInput').prop("disabled", !$('#activatedCheckbox').prop("checked"));
+        $('#tagInput').prop("disabled", !$('#activatedCheckbox').prop("checked"));
       });
     });
 })();
